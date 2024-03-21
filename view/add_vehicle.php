5e3850e450880c8257e656212a8c1db9077e4c9d
kpnguyen02 <112967218+kpnguyen02@users.noreply.github.com>
2024-03-20 23:48:22 -0500

    Update add_vehicle.php

--- view/add_vehicle.php
+++ view/add_vehicle.php
@@ -1,11 +1,11 @@
 <?php
 // Connects to the ZippyUsedAutos database
-$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-$username = 'root';
-//$password = 'password';
+$dsn = 'mysql:host=etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=et6jp5phfggu6kos';
+$username = 'pqzirqysdzixmwdq';
+$password = 'yfl4d7mg70coaxpw';
 
 try {
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 
 } catch (PDOException $e) {
     $error = "Database Error: ";
